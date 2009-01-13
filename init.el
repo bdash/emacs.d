b7dcf909770ad8a3427f5b4425abb2dac77883c8
Mark Rowe <mrowe@apple.com>
2009-01-12 19:40:13 -0800

    Disable Slime-related stuff for now as it's not used and isn't available on all machines.

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
 (setq dotfiles-dir (file-name-directory
                     (or (buffer-file-name) load-file-name)))
 (add-to-list 'load-path dotfiles-dir)
-(add-to-list 'load-path "~/Documents/Source/CVS/slime")
 
 ; Tell emacs in two different ways that it may need to look in /usr/local/bin to find git
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
@@ -11,11 +10,14 @@
 (require 'webkit)
 (require 'webkit-merging)
 
-(setf common-lisp-hyperspec-root "file:/Volumes/Data/Users/mrowe/Downloads/HyperSpec-7-0/HyperSpec/")
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
 
-(require 'slime)
 
+; (add-to-list 'load-path "~/Documents/Source/CVS/slime")
+; (require 'slime)
+;
+; (setf common-lisp-hyperspec-root "file:/Volumes/Data/Users/mrowe/Downloads/HyperSpec-7-0/HyperSpec/")
+; (setq inferior-lisp-program "/usr/local/bin/sbcl")
+;
 ; (add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
 ; (add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
 ; 
