c1cee6c339392f4a2a2356695666bf71b68df965
Mark Rowe <mrowe@apple.com>
2013-01-09 03:34:29 -0800

    Load and configure packages only when support is available.

--- init.el
+++ init.el
@@ -6,19 +6,62 @@
 (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 (add-to-list 'exec-path "/usr/local/bin")
 
-(require 'util)
-(require 'cl)
-(require 'webkit)
-(require 'webkit-merging)
-(require 'color-theme)
-(setq color-theme-is-global t)
-(color-theme-initialize)
-(color-theme-wombat)
+(when (require 'package nil t)
+  (add-to-list 'package-archives
+               '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (package-initialize)
+
+  (when (not package-archive-contents)
+    (package-refresh-contents))
+
+  (defvar my-packages '(color-theme
+                        clojure-mode
+                        clojure-test-mode
+                        nrepl
+                        paredit
+                        rainbow-delimiters
+                        auto-complete
+                        ac-nrepl))
+  (dolist (p my-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(when (>= emacs-major-version 24)
+  (load-theme 'wombat t))
+
 (ido-mode)
 
 (setq-default indent-tabs-mode nil
               make-backup-files nil
-              split-width-threshold nil)
+              split-width-threshold nil
+              nrepl-popup-stacktraces nil)
+
+(when (require 'paredit nil t)
+  (add-hook 'nrepl-mode-hook 'paredit-mode)
+  (add-hook 'clojure-mode-hook 'paredit-mode))
+
+(when (require 'auto-complete nil t)
+  (add-hook 'nrepl-mode-hook 'auto-complete-mode)
+  (add-hook 'clojure-mode-hook 'auto-complete-mode))
+
+(when (require 'rainbow-delimiters nil t)
+  (add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
+  (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode))
+
+(when (require 'ac-nrepl nil t)
+  (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+  (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+  (eval-after-load "auto-complete"
+    '(add-to-list 'ac-modes 'nrepl-mode))
+
+  (defun set-auto-complete-as-completion-at-point-function ()
+    (setq completion-at-point-functions '(auto-complete)))
+  (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+
+  (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+  (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function))
+
+
 
 (defun my-c-mode-common-hook ()
   (setq tab-width 8
@@ -27,10 +70,10 @@
 (add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
 
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(add-log-full-name "Mark Rowe")
  '(add-log-mailing-address "mrowe@apple.com")
  '(change-log-version-number-regexp-list (list "Merge r\\([0-9]+\\)"))
@@ -43,27 +86,18 @@
  '(vc-handled-backends (quote nil)))
 
 (custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(default ((t (:height 100)))))
 
 (set-face-background 'region "grey20")
 
-(require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
-
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-(defvar my-packages '(clojure-mode
-                      nrepl))
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(require 'util)
+(require 'cl)
+(require 'webkit)
+(require 'webkit-merging)
 
 (defvar per-host-init-file (concat (file-name-directory (locate-library "init")) "hosts/" (system-name) ".el"))
 (message per-host-init-file)
